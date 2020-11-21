c1356f679a89ad8ef5bb4297ffa1e9df217b6651
sgkul2000 <sgkul2000@users.noreply.github.com>
2020-11-21 22:18:40 +0000

    deploy: 2ae3fcd4e637f19cc13211c8365fb894d6aef4c4

---
+++ precache-manifest.bf01fdb75595eccaf86cdd93ebf5b426.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226119aa5eff1613462b",
+    "url": "/css/app.08a0b0b3.css"
+  },
+  {
+    "revision": "aff1161b77af83517740f69ba90eb6e6",
+    "url": "/img/circle-outline.aff1161b.svg"
+  },
+  {
+    "revision": "3a760e5c5393475b6477d4295806f06e",
+    "url": "/img/circle.3a760e5c.svg"
+  },
+  {
+    "revision": "5a226e36b139f8e3415d5de7358db0ed",
+    "url": "/img/delete.5a226e36.svg"
+  },
+  {
+    "revision": "a5cdd35d57642abdff642241b49bbfbc",
+    "url": "/img/plus.a5cdd35d.svg"
+  },
+  {
+    "revision": "11e7740de4de641c800a7dd5744d6b32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "226119aa5eff1613462b",
+    "url": "/js/app.7b70e5b9.js"
+  },
+  {
+    "revision": "bbbbd7e5da61c2911d88",
+    "url": "/js/chunk-vendors.d6f91d6d.js"
+  },
+  {
+    "revision": "570497c67a7c13acf56d20e6aeb5919a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
